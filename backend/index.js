924e2dd18e458667fe923ad5b255bb2f921001a3
Juan David Conrado Pertuz <djconrado@uninorte.edu.co>
2019-10-17 13:54:24 -0500

    Prueba 1

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
-const { mongoose} = require ('./db');
+const { mongoose}  = require ('./db');
 
 // Settings
 app.set('port',process.env.PORT || 3000); // usar el puerto que le asigne el sistema, si este no agina alguno, usa el 3000
