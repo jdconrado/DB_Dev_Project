81438cb3b126e8abec02498950669b42d6ead454
Juan David Conrado Pertuz <djconrado@uninorte.edu.co>
2019-11-01 11:34:06 -0500

    Creación de HomePage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: '-', component: LoginComponent }
 ];
