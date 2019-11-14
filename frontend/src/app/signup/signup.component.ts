bfc75fa10776c57015e66f721e0633f10feb9b0d
Moisesdd <moisesdd@uninorte.edu.co>
2019-11-14 08:54:48 -0500

    Sign up pide descripcion para vendedor

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  show = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  myFunction(event) {
+    console.log(event);
+    console.log(event.srcElement.addEventListener.name);
+    console.log(event.srcElement.value)
+    if (event.srcElement.value === 'cliente') {
+      this.show = false;
+    }
+    if (event.srcElement.value === 'vendedor') {
+      this.show = true;
+    }
+
   }
 
 }
