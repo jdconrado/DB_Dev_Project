dc1347ee7810dea5cca4f9750c441f75d9a1d2bd
Moisesdd <moisesdd@uninorte.edu.co>
2019-11-16 21:42:33 -0500

    Campos de registro y post fallido

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,21 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
   show = false;
-  constructor() { }
+  us: any;
+  constructor(private uservice: UsersService) {
+    this.us = [];
+
+  }
 
   ngOnInit() {
+
   }
   myFunction(event) {
     console.log(event);
     console.log(event.srcElement.addEventListener.name);
-    console.log(event.srcElement.value)
+    console.log(event.srcElement.value);
     if (event.srcElement.value === 'cliente') {
       this.show = false;
     }
     if (event.srcElement.value === 'vendedor') {
       this.show = true;
     }
-
+  }
+  saveData(name, lastname, phone, email, DateOfBirth, password, inputConfirmPassword, inputDesc): void {
+    this.us = { name, lastname, phone, email, DateOfBirth, password, inputConfirmPassword, inputDesc };
+    this.uservice.PostUsers(this.us);
   }
 
 }
--- frontend/src/app/vendor/vendor.component.ts
+++ frontend/src/app/vendor/vendor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { USUARIOS } from '../users';
 import { FOTOS } from '../vendor/fotos';
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
@@ -9,9 +10,18 @@ import { FOTOS } from '../vendor/fotos';
 export class VendorComponent implements OnInit {
   usuarios = USUARIOS;
   fotos = FOTOS;
-  constructor() { }
+  usuario: any;
+  constructor(private usuarioss: UsersService) {
+    this.usuario = [];
+
+  }
 
   ngOnInit() {
+    this.usuarioss.getUsers()
+      .then((resultado: any) => {
+        this.usuario = resultado;
+      })
+      .catch(err => console.log('Error', err));
   }
 
 }
